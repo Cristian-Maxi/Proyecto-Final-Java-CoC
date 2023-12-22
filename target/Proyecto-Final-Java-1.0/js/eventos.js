1d17e02d732a4898081f843176ab9ca471d0ea20
LaPiragna <gonzalovarelaalagna@gmail.com>
2023-12-21 23:41:49 -0300

    Últimos cambios: Se agregó un nuevo archivo para la última sección de los monumentos para que pueda ser interactiva y responsive, Se modificaron algunas cuestiones en el archivo de la lógica de los precios ya que no estaban funcionando correctamente, Se hicieron los cambios correspondientes en el index.html para adecuar el contenido a las modificaciones con respecto aĺ código incorporado en js, Se modificaron algunos estilos para adecuar la disposición de los elementos con respecto a las nuevas funcionalidades.

--- target/Proyecto-Final-Java-1.0/js/eventos.js
+++ target/Proyecto-Final-Java-1.0/js/eventos.js
@@ -1,25 +1,42 @@
-// Variable para almacenar el id del contenedor que se ha clicado
-var clicked = null;
+$(document).ready(function() {
+  // Variable para almacenar el id del contenedor que se ha clicado
+  var clicked = null;
 
-// Función para mostrar el párrafo y ocultar los otros contenedores cuando se hace clic en la imagen de un contenedor
-$(".container img").click(function(event) {
-  event.stopPropagation();
-  var id = $(this).parent().attr("id"); // Obtiene el id del contenedor que se ha clicado
-  if (clicked !== id) {
-    $(".container > *").css("display", "none"); // Oculta todos los elementos de todos los contenedores
-    $(this).siblings().css("display", "block"); // Muestra el párrafo del contenedor que se ha clicado
-    $(".container").not("#" + id).css("display", "none"); // Oculta los otros contenedores
-    clicked = id; // Guarda el id del contenedor que se ha clicado
-  } else {
-    $(".container > *").css("display", "none"); // Oculta todos los elementos de todos los contenedores
-    $(".container").css("display", "block"); // Muestra todos los contenedores
-    clicked = null; // Restablece la variable
-  }
-});
+  // Función para mostrar el párrafo y ocultar los otros contenedores cuando se hace clic en la imagen de un contenedor
+  $(".container img").click(function(event) {
+    event.stopPropagation();
+    var id = $(this).parent().attr("id"); // Obtiene el id del contenedor que se ha clicado
+    if (clicked !== id) {
+      $(".container").fadeOut(500); // Oculta todos los contenedores con un efecto de desvanecimiento
+      $("#" + id).fadeIn(500); // Muestra el contenedor que se ha clicado con un efecto de aparición gradual
+      $("#" + id + " img").show(); // Muestra la imagen del contenedor que se ha clicado
+      $("#" + id + " p").show(); // Muestra el párrafo del contenedor que se ha clicado instantáneamente
+      clicked = id; // Guarda el id del contenedor que se ha clicado
+      if (window.matchMedia("(max-width: 420px)").matches) {
+        $("#monumentos").css("margin-bottom", "10px"); // Reduce el margin-bottom del elemento h2 con id "monumentos" a 0
+      }
+    } else {
+      $(".container").fadeIn(500); // Muestra todos los contenedores con un efecto de aparición gradual
+      $(".container p").hide(); // Oculta todos los párrafos instantáneamente
+      clicked = null; // Restablece la variable
+      if (window.matchMedia("(max-width: 420px)").matches) {
+        $("#monumentos").css("margin-bottom", "250px"); // Reestablece el margin-bottom del elemento h2 con id "monumentos" a 250px
+      }
+    }
+  });
 
-// Función para ocultar los párrafos y mostrar todos los contenedores cuando se hace clic en cualquier parte de la página
-$(document).click(function() {
-  $(".container > *").css("display", "none"); // Oculta todos los elementos de todos los contenedores
-  $(".container").css("display", "block"); // Muestra todos los contenedores
-  clicked = null; // Restablece la variable
+  // Función para ocultar los párrafos y mostrar todos los contenedores cuando se hace clic en cualquier parte de la página
+  $(document).click(function() {
+    if (clicked !== null) { // Solo se ejecuta si previamente se ha hecho clic en una imagen
+      $(".container").fadeIn(500); // Muestra todos los contenedores con un efecto de aparición gradual
+      $(".container p").hide(); // Oculta todos los párrafos instantáneamente
+      clicked = null; // Restablece la variable
+      if (window.matchMedia("(max-width: 420px)").matches) {
+        $("#monumentos").css("margin-bottom", "250px"); // Reestablece el margin-bottom del elemento h2 con id "monumentos" a 250px
+      }
+	  else {
+		  $("#monumentos").css("margin-bottom", "10px");
+	  }
+    }
+  });
 });
